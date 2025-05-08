25c734608597abb3c94a8c8d90fed945ec9b3adc
Harry Ore Lara <harryorelara@gmail.com>
2025-05-08 11:13:56 -0500

    feat: Refactor AppEffectsArray for better readability y añadir archivos de acciones, efectos y selectores para cliente

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -1,9 +1,11 @@
+import { SharedAccesosEffectsArray } from './store/accesos/accesos.effects';
+import { AuthEffects } from './store/auth/effects/auth.effects';
+import { SharedMaestrosEffectsArray } from './store/maestros/maestros.effects';
+import { SharedEffectsArray } from './store/shared/shared.effects';
 
-import { SharedAccesosEffectsArray } from "./store/accesos/accesos.effects";
-import { AuthEffects } from "./store/auth/effects/auth.effects";
-import { SharedMaestrosEffectsArray } from "./store/maestros/maestros.effects";
-import { SharedEffectsArray } from "./store/shared/shared.effects";
-
-
-
-export const AppEffectsArray:any[] = [...SharedEffectsArray, ...SharedAccesosEffectsArray, ...SharedMaestrosEffectsArray, AuthEffects];
+export const AppEffectsArray: any[] = [
+  ...SharedEffectsArray,
+  ...SharedAccesosEffectsArray,
+  ...SharedMaestrosEffectsArray,
+  AuthEffects,
+];
